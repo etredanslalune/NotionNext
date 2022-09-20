8e7fec86bd2fa54af093b1202dfcaecd3cccb9bf
être dans la lune <68406741+etredanslalune@users.noreply.github.com>
2022-09-20 12:07:34 -0400

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@
 const BLOG = {
   AUTHOR: '杯子', // 作者
   BIO: '一觉游仙好梦，任它竹冷松寒。', // 作者简介
-  LINK: 'https://notion-next-96jjs5zlj-etredanslalune.vercel.app', // 网站地址
+  LINK: 'https://notion-next-navy-mu.vercel.app', // 网站地址
   KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
   NOTION_PAGE_ID:
   process.env.NOTION_PAGE_ID || '95f06f484f1f4977bf81bb7c42c2f398', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
